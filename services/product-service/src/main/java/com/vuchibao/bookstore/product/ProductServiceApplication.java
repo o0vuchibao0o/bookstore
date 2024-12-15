f547f3d64bd991f1f58046e768d972864ce93443
Vu Chi Bao <vuchibao710@gmail.com>
2024-12-15 21:09:01 +0700

    Setup Product Service

--- services/product-service/src/main/java/com/vuchibao/bookstore/product/ProductServiceApplication.java
+++ services/product-service/src/main/java/com/vuchibao/bookstore/product/ProductServiceApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class ProductServiceApplication {
+public
+class
+ProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
